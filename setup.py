b7036d99f2d3b14f0d159a389b45e1537c3fd6d9
Tanmay Deshpande <dsp.tanmay@gmail.com>
2020-08-07 08:44:03 +0530

    made setup into function

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
-import pkg_resources
-import subprocess as sp
 import sys
 
-python = sys.executable
-installed = {pkg.key for pkg in pkg_resources.working_set}
-required = {"questionary", "tabulate"}
+import pkg_resources
+import subprocess as sp
 
-missing = required - installed
 
-if missing:
-    sp.check_call(
-        [python, "-m", "pip", "--install", "--upgrade", "--no-cache-dir", *missing],
-        stdout=sp.DEVNULL,
-    )
+def setup():
+
+    python = sys.executable
+    installed = {pkg.key for pkg in pkg_resources.working_set}
+    required = {"questionary", "tabulate"}
+
+    missing = required - installed
+
+    if missing:
+        sp.check_call(
+            [python, 
+             "-m", 
+             "pip", 
+             "--install", 
+             "--upgrade", 
+             "--no-cache-dir", 
+             *missing],
+            stdout=sp.DEVNULL,
+        )
